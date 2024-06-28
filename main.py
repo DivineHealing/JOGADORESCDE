0f46ae80db44faba04dc21e334af903045edfa91
DivineHealing <biel_ariel@hotmail.com>
2024-06-27 21:41:45 -0300

    Sistema para jogadores do CdE
    Em desenvolvimento

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, render_template, request
+
+main = Flask(__name__)
+
+@main.route("/")
+def home():
+    return render_template('home.html')
+
+# CODIGO AQUI
+
+
+
+# NAO PASSAR DAQUI
+
+if __name__ == '__main__':
+    main.run(debug=True)
+
