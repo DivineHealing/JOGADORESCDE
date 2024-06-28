0871e564a71677b8fcad7cf15225a9f423728d77
Andrey Santos <andrey.jhonnes1997@gmail.com>
2024-06-28 09:48:13 -0300

    adicaoatt
    atualizei para mostrar o restante dos atributos

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ def atrstatus():
     forca = float(data.get('forca'))
     destreza =  float(data.get('destreza'))
     inteligencia = float(data.get('inteligencia'))
+    determinacao = float(data['determinacao'])
+    percepcao = float(data['percepcao'])
+    carisma = float(data['carisma'])
+    print(data)
 
     forcae = float(data.get('forcaE')) # Pega o valor do equipamento
     forca += forcae # Soma o valor base com o Equipamento
 
-    return jsonify(forca=forca, destreza=destreza, inteligencia=inteligencia) # AQUI VAI JOGAR O DADO PRO FRONT
+    return jsonify(forca=forca, destreza=destreza, inteligencia=inteligencia, determinacao=determinacao, percepcao=percepcao,
+                   carisma=carisma) # AQUI VAI JOGAR O DADO PRO FRONT
 
 # Fim do Codigo
 if __name__ == '__main__': 
--- static/script.js
+++ static/script.js
@@ -2,6 +2,9 @@ function alterar() { // função que ativa quando aperta o botão
     const forca = parseFloat(document.getElementById('forca').value) || 0; // Declaração das variaveis que serão processadas, caso nao tiver, o valor sera zero.
     const destreza = parseFloat(document.getElementById('destreza').value) || 0;
     const inteligencia = parseFloat(document.getElementById('inteligencia').value) || 0;
+    const determinacao =  parseFloat(document.getElementById('determinacao').value) || 0;
+    const percepcao = parseFloat(document.getElementById('percepcao').value) || 0;
+    const carisma = parseFloat(document.getElementById('carisma').value) || 0;
     const forcaE =parseFloat(document.getElementById('forcaE').value) || 0;
     
 
@@ -10,13 +13,18 @@ function alterar() { // função que ativa quando aperta o botão
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ forca: forca, destreza:destreza, inteligencia:inteligencia, forcaE: forcaE}) // Variaveis que serão jogadas para o Python 
+        body: JSON.stringify({ forca: forca, destreza: destreza, inteligencia: inteligencia, forcaE: forcaE, 
+            determinacao: determinacao, percepcao: percepcao, carisma: carisma
+        }) // Variaveis que serão jogadas para o Python 
     })
     .then(response => response.json())
     .then(data => {
         document.getElementById('forcat').textContent = `${data.forca.toLocaleString('pt-BR')}`, // Esses aqui são o que joga para o Front, na tela do HTML
         document.getElementById('destrezat').textContent = `${data.destreza.toLocaleString('pt-BR')}`,
-        document.getElementById('inteligenciat').textContent = `${data.inteligencia.toLocaleString('pt-BR')}` 
+        document.getElementById('inteligenciat').textContent = `${data.inteligencia.toLocaleString('pt-BR')}`,
+        document.getElementById('determinacaot').textContent = `${data.determinacao.toLocaleString('pt-BR')}`,
+        document.getElementById('percepcaot').textContent = `${data.percepcao.toLocaleString('pt-BR')}`,
+        document.getElementById('carismat').textContent = `${data.percepcao.toLocaleString('pt-BR')}`
         ;
     })
     .catch(error => {
