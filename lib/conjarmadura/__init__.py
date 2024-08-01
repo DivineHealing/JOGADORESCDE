c3e4169d01582b87ee141fbb05e83f3ddd812c6b
Andrey Santos <andrey.jhonnes1997@gmail.com>
2024-08-01 19:26:59 -0300

    ajustando classe armadura

--- lib/conjarmadura/__init__.py
+++ lib/conjarmadura/__init__.py
@@ -9,7 +9,12 @@ class ConjArmadura:
     calca: Equipamento
     bota: Equipamento
 
-    def somarEquip(self, escolha1, escolha2 ):  # pegara o total do atributo escolhido em todos os equipamentos       
+    def somarEquip(self, escolha):  # pegara o total do atributo escolhido em todos os equipamentos
+        total = (self.elmo.att.pegaValor(escolha) + self.peitoral.att.pegaValor(escolha) + self.luva.att.pegaValor(escolha) + self.calca.att.pegaValor(escolha) + 
+                 self.bota.att.pegaValor(escolha))
+        return total
+    
+    def somarEspeCJA(self, escolha1, escolha2 ):  # pegara o total do tipo de elemento escolhido em todos os equipamentos       
         total = (self.elmo.pegaInfo(escolha1).pegaTipo(escolha2) + self.peitoral.pegaInfo(escolha1).pegaTipo(escolha2) + 
                  self.luva.pegaInfo(escolha1).pegaTipo(escolha2) + self.calca.pegaInfo(escolha1).pegaTipo(escolha2) + 
                  self.bota.pegaInfo(escolha1).pegaTipo(escolha2))
