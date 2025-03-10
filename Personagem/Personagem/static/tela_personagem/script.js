85c5e42823f6b206294d1adc76c3e0a0d285ef06
DivineHealing <biel_ariel@hotmail.com>
2025-03-10 19:12:35 -0300

    Trampo

--- Personagem/Personagem/static/tela_personagem/script.js
+++ Personagem/Personagem/static/tela_personagem/script.js
@@ -5,18 +5,22 @@ function selecionarPersonagem() {
 }
 
 const atributos = [
-    { nome: "forca", containerId:"forca_container", bonusElementId: "forcaBonus" },
-    { nome: "destreza", containerId:"destreza_container", bonusElementId: "destrezaBonus" },
-    { nome: "inteligencia", containerId:"inteligencia_container", bonusElementId: "inteligenciaBonus" },
-    { nome: "determinacao", containerId:"determinacao_container", bonusElementId: "determinacaoBonus" },
-    { nome: "perspicacia", containerId:"perspicacia_container", bonusElementId: "perspicaciaBonus" },
-    { nome: "carisma", containerId:"carisma_container", bonusElementId: "carismaBonus" },
+    { nome: "regenVida", containerId:"regenVida_container"},
+    { nome: "regenMana", containerId:"regenMana_container"},
+    { nome: "regenVigor", containerId:"regenVigor_container"},
 
-    { nome: "danoFixo_1", containerId: "danoFixo1_container" },
+    { nome: "forca", containerId:"forca_container", bonusElementId: "forcaBonus" },
+    { nome: "destreza", containerId:"destreza_container", bonusElementId: "destrezaBonus"},
+    { nome: "inteligencia", containerId:"inteligencia_container", bonusElementId: "inteligenciaBonus"},
+    { nome: "determinacao", containerId:"determinacao_container", bonusElementId: "determinacaoBonus"},
+    { nome: "perspicacia", containerId:"perspicacia_container", bonusElementId: "perspicaciaBonus"},
+    { nome: "carisma", containerId:"carisma_container", bonusElementId: "carismaBonus"},
+
+    { nome: "danoFixo_1", containerId: "danoFixo1_container"},
     { nome: "penetracao_1", containerId: "penetracao1_container"},
-    { nome: "danoFixo_2", containerId: "danoFixo2_container" },
+    { nome: "danoFixo_2", containerId: "danoFixo2_container"},
     { nome: "penetracao_2", containerId: "penetracao2_container"},
-    { nome: "danoFixo_3", containerId: "danoFixo3_container" },
+    { nome: "danoFixo_3", containerId: "danoFixo3_container"},
     { nome: "penetracao_3", containerId: "penetracao3_container"},
 
     { nome: "defesaFixa_1", containerId: "defesaFixa1_container" },
@@ -30,21 +34,48 @@ const atributos = [
     { nome: "defesaFixa_5", containerId: "defesaFixa5_container" },
     { nome: "resistencia_5", containerId: "resistencia5_container"},
 
-    { nome: "rolagem1", containerId: "rolagem1_container" },
+    { nome: "rolagem1", containerId: "rolagem1_container"},
     { nome: "rolagem2", containerId: "rolagem2_container"},
-    { nome: "rolagem3", containerId: "rolagem3_container" },
+    { nome: "rolagem3", containerId: "rolagem3_container"},
     { nome: "rolagem4", containerId: "rolagem4_container"},
-    { nome: "rolagem5", containerId: "rolagem5_container" },
+    { nome: "rolagem5", containerId: "rolagem5_container"},
     { nome: "rolagem6", containerId: "rolagem6_container"},
-    { nome: "rolagem7", containerId: "rolagem7_container" },
+    { nome: "rolagem7", containerId: "rolagem7_container"},
     { nome: "rolagem8", containerId: "rolagem8_container"},
-    { nome: "rolagem9", containerId: "rolagem9_container" },
+    { nome: "rolagem9", containerId: "rolagem9_container"},
     { nome: "rolagem10", containerId: "rolagem10_container"},
-    { nome: "rolagem11", containerId: "rolagem11_container" },
+    { nome: "rolagem11", containerId: "rolagem11_container"},
     { nome: "rolagem12", containerId: "rolagem12_container"},
-    { nome: "rolagem13", containerId: "rolagem13_container" },
+    { nome: "rolagem13", containerId: "rolagem13_container"},
     { nome: "rolagem14", containerId: "rolagem14_container"},
-    { nome: "rolagem15", containerId: "rolagem15_container" },
+    { nome: "rolagem15", containerId: "rolagem15_container"},
+    { nome: "rolagem15", containerId: "rolagem15_container"},
+    { nome: "rolagem16", containerId: "rolagem16_container"},
+    { nome: "rolagem17", containerId: "rolagem17_container"},
+    { nome: "rolagem18", containerId: "rolagem18_container"},
+    { nome: "rolagem19", containerId: "rolagem19_container"},
+    { nome: "rolagem20", containerId: "rolagem20_container"},
+    { nome: "rolagem21", containerId: "rolagem21_container"},
+    { nome: "rolagem22", containerId: "rolagem22_container"},
+    { nome: "rolagem23", containerId: "rolagem23_container"},
+    { nome: "rolagem24", containerId: "rolagem24_container"},
+    { nome: "rolagem25", containerId: "rolagem25_container"},
+
+    { nome: "amplificacao1", containerId: "amplificacao1_container" },
+    { nome: "amplificacao2", containerId: "amplificacao2_container"},
+    { nome: "amplificacao3", containerId: "amplificacao3_container" },
+    { nome: "amplificacao4", containerId: "amplificacao4_container"},
+    { nome: "amplificacao5", containerId: "amplificacao5_container" },
+    { nome: "amplificacao6", containerId: "amplificacao6_container"},
+    { nome: "amplificacao7", containerId: "amplificacao7_container" },
+    { nome: "amplificacao8", containerId: "amplificacao8_container"},
+    { nome: "amplificacao9", containerId: "amplificacao9_container" },
+    { nome: "amplificacao10", containerId: "amplificacao10_container"},
+    { nome: "amplificacao11", containerId: "amplificacao11_container" },
+    { nome: "amplificacao12", containerId: "amplificacao12_container"},
+    { nome: "amplificacao13", containerId: "amplificacao13_container" },
+    { nome: "amplificacao14", containerId: "amplificacao14_container"},
+    { nome: "amplificacao15", containerId: "amplificacao15_container" },
 
     
 ];
@@ -74,7 +105,7 @@ function formatarAtributo(atributo) {
         let bonus = valor / 100;
         bonus = bonus.toFixed(2);
         bonus = Math.trunc(bonus); // Remove as casas decimais (arredonda para baixo)
-        bonus = "| " + bonus; // Adiciona o sinal de mais
+        bonus = "——————— +" +bonus; // Adiciona o sinal de mais
 
         bonusElement.innerText = bonus; // Exibe o bônus
     }
