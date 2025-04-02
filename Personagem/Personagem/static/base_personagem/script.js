e09119c5eca72f2b6a42feea87d315b546c158cb
DivineHealing <biel_ariel@hotmail.com>
2025-04-02 14:23:12 -0300

    Atualização

--- Personagem/Personagem/static/base_personagem/script.js
+++ Personagem/Personagem/static/base_personagem/script.js
@@ -12,7 +12,7 @@ addDefesaBtn.addEventListener('click', function () {
     defesaCount++; // Increment counter for each new attribute row
 
     const atributoRow = document.createElement('div');
-    if (defesaCount <= 7) {
+    if (defesaCount <= 3) {
         atributoRow.classList.add('defesa-row');
         atributoRow.innerHTML = `
             <div>
@@ -41,7 +41,7 @@ addAtributoBtn.addEventListener('click', function () {
     danoCount++; // Increment counter for each new attribute row
 
     const atributoRow = document.createElement('div');
-    if (danoCount <= 7) {
+    if (danoCount <= 3) {
         atributoRow.classList.add('dano-row');
         atributoRow.innerHTML = `
             <div>
@@ -115,11 +115,11 @@ addAmplificacaoBtn.addEventListener('click', function () {
 });
 
 
-const addHab1Btn = document.getElementById('addRegeneracao');
+const addRegBtn = document.getElementById('addRegeneracao');
 const regeneracaoContainer = document.getElementById('regeneracaoContainer');
 let regeneracaoCount = 0; // Counter to keep track of attribute rows
 
-addHab1Btn.addEventListener('click', function () {
+addRegBtn.addEventListener('click', function () {
     regeneracaoCount++; // Increment counter for each new attribute row
 
     const regeneracaoRow = document.createElement('div');
@@ -128,58 +128,15 @@ addHab1Btn.addEventListener('click', function () {
         regeneracaoRow.innerHTML = `
             <div>
                 <label for="elemento${regeneracaoCount}">Tipo</label>
-                <input type="text" id="regeneracaoTipo${regeneracaoCount}" name="regeneracaoTipo${regeneracaoCount}" placeholder="Ex: Vida">
+                <select class='personagem_select' id="regeneracaoTipo${regeneracaoCount}" name="regeneracaoTipo${regeneracaoCount}">                        
+                    <option value="regenVida" selected>Vida</option>
+                    <option value="regenMana">Mana</option>
+                    <option value="regenVigor">Vigor</option>
+                </select>
             </div>
             <div>
                 <label for="dano${regeneracaoCount}">Valor</label>
                 <input type="number" id="regeneracao${regeneracaoCount}" name="regeneracao${regeneracaoCount}" value="0">
-            </div>
-        `;
-
-        regeneracaoContainer.appendChild(regeneracaoRow);
-    }
-});
-
-
-
-const addHab1Btn = document.getElementById('addHab1');
-const hab1Container = document.getElementById('hab1Container');
-let hab1Count = 2; // Counter to keep track of attribute rows
-
-addHab1Btn.addEventListener('click', function () {
-    hab1Count++; // Increment counter for each new attribute row
-
-    const regeneracaoRow = document.createElement('div');
-    if (hab1Count <= 6) {
-        regeneracaoRow.classList.add('habilidade-row');
-        regeneracaoRow.innerHTML = `
-            <div class='habilidade-row-container'>
-                <div class='habilidade-row-miniContainer'>
-                    <label for="habilidadeTipo${habilidadesCount}">Tipo do habilidade</label>
-                    <select class='personagem_select' id="habilidadeTipo${habilidadesCount}" name="habilidadeTipo${habilidadesCount}">
-                        <option value="habilidadePassivo" selected> habilidade Passivo</option>
-                        <option value="habilidadeAtivo"> habilidade Ativo</option>
-                        <option value="habilidadeAura"> habilidade Aura</option>
-                    </select>
-                    <label for="habilidadeNivel${habilidadesCount}">Nível do habilidade</label>
-                    <select class='personagem_select' id="habilidadeNivel${habilidadesCount}" name="habilidadeNivel${habilidadesCount}">                        
-                        <option value="nivel2" selected>Nível 2</option>
-                        <option value="nivel3">Nível 3</option>
-                        <option value="nivel4">Nível 4</option>
-                        <option value="nivel5">Nível 5</option>
-                        <option value="nivel6">Nível 6</option>
-                    </select>
-                </div>
-                <div>
-                    <label for="habilidadeNome${habilidadesCount}">Nome do habilidade:</label>
-                    <input style="width: 100%"; type="text" id="habilidadeNome${habilidadesCount}" name="habilidadeNome${habilidadesCount}" placeholder="Nome do habilidade">
-                    <label for="habilidadeCusto${habilidadesCount}">Custo do habilidade:</label>
-                    <input style="width: 50%"; type="text" id="habilidadeCusto${habilidadesCount}" name="habilidadeCusto${habilidadesCount}" placeholder="Custo do habilidade">
-                </div class='habilidade-row-miniContainer'>
-                <div class='habilidade-row-miniContainer'>
-                    <label for="habilidadeDesc${habilidadesCount}">Descrição do habilidade:</label>
-                    <textarea style="width: 100%; height: 10em; wrap: "hard""; type="text" id="habilidadeDesc${habilidadesCount}" name="habilidadeDesc${habilidadesCount}" placeholder="Descrição do habilidade"></textarea>
-                </div>
             </div>
         `;
 
--- Personagem/base_personagem/models.py
+++ Personagem/base_personagem/models.py
@@ -1,15 +1,77 @@
 from django.db import models
 
 class Base_personagem(models.Model):
-    nome = models.CharField(max_length=30, default="")  # Valor padrão para começar
-    vida = models.IntegerField(default="0")
-    mana = models.IntegerField(default="0")
-    vigor = models.IntegerField(default="0")
-    forca = models.IntegerField(default="0")
-    destreza = models.IntegerField(default="0")
-    inteligencia = models.IntegerField(default="0")
-    determinacao = models.IntegerField(default="0")
-    perspicacia = models.IntegerField(default="0")
-    carisma = models.IntegerField(default="0")
+    personagem = models.CharField(max_length=100, default="")
+
+    # ATRIBUTOS e REGENERAÇÃO
+    vida = models.IntegerField(default=0, blank=True)
+    regenVida = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    mana = models.IntegerField(default=0, blank=True)
+    regenMana = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    vigor = models.IntegerField(default=0, blank=True)
+    regenVigor = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+
+    # STATUS
+    forca = models.IntegerField(default=0, blank=True)
+    destreza = models.IntegerField(default=0, blank=True)
+    inteligencia = models.IntegerField(default=0, blank=True)
+    determinacao = models.IntegerField(default=0, blank=True)
+    perspicacia = models.IntegerField(default=0, blank=True)
+    carisma = models.IntegerField(default=0, blank=True)
+
+    # ROLAGEM
+    tipoRolagem_1 = models.CharField(max_length=100, default="")
+    tipoRolagem_2 = models.CharField(max_length=100, default="")
+    tipoRolagem_3 = models.CharField(max_length=100, default="")
+    tipoRolagem_4 = models.CharField(max_length=100, default="")
+    tipoRolagem_5 = models.CharField(max_length=100, default="")
+    rolagem_1 = models.IntegerField(default=0, blank=True)
+    rolagem_2 = models.IntegerField(default=0, blank=True)
+    rolagem_3 = models.IntegerField(default=0, blank=True)
+    rolagem_4 = models.IntegerField(default=0, blank=True)
+    rolagem_5 = models.IntegerField(default=0, blank=True)
+
+    # DEFESA
+    elementoDefesa_1 = models.CharField(max_length=100, default="")
+    elementoDefesa_2 = models.CharField(max_length=100, default="")
+    elementoDefesa_3 = models.CharField(max_length=100, default="")
+    elementoResistencia_1 = models.CharField(max_length=100, default="")
+    elementoResistencia_2 = models.CharField(max_length=100, default="")
+    elementoResistencia_3 = models.CharField(max_length=100, default="")
+    defesaFixa_1 = models.IntegerField(default=0, blank=True)
+    defesaFixa_2 = models.IntegerField(default=0, blank=True)
+    defesaFixa_3 = models.IntegerField(default=0, blank=True)
+    resistencia_1 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    resistencia_2 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    resistencia_3 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    reducao = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+
+    # DANO
+    elementoDano_1 = models.CharField(max_length=100, default="")
+    elementoDano_2 = models.CharField(max_length=100, default="")
+    elementoDano_3 = models.CharField(max_length=100, default="")
+    elementoPenetracao_1 = models.CharField(max_length=100, default="")
+    elementoPenetracao_2 = models.CharField(max_length=100, default="")
+    elementoPenetracao_3 = models.CharField(max_length=100, default="")
+    danoFixo_1 = models.IntegerField(default=0, blank=True)
+    danoFixo_2 = models.IntegerField(default=0, blank=True)
+    danoFixo_3 = models.IntegerField(default=0, blank=True)
+    penetracao_1 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    penetracao_2 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    penetracao_3 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    danoFinal = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+
+    # AMPLIFICAÇÃO
+    elementoAmplificacao_1 = models.CharField(max_length=100, default="")
+    elementoAmplificacao_2 = models.CharField(max_length=100, default="")
+    elementoAmplificacao_3 = models.CharField(max_length=100, default="")
+    elementoAmplificacao_4 = models.CharField(max_length=100, default="")
+    elementoAmplificacao_5 = models.CharField(max_length=100, default="")
+    amplificacao_1 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    amplificacao_2 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    amplificacao_3 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    amplificacao_4 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    amplificacao_5 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+
     def __str__(self):
-        return self.nome
+        return self.personagem
